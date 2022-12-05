c6b070db16a14c1f97faded379e3b862e86f415e
ravipandey1298 <68158316+ravipandey1298@users.noreply.github.com>
2022-12-05 18:45:02 +0530

    Update DateTime.js
    Update DateTime using useEffect Hooks

--- src/components/DateTime.js
+++ src/components/DateTime.js
@@ -1,12 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DateTimePicker from "react-datetime-picker";
 
 function DateTime(props) {
   const [value, onChange] = useState(new Date());
   //   const minDate = new Date();
   //   const maxDate = new Date("02/06/2021 06:00 PM");
-  props.setProgress(value);
-  //  console.log(value);
+  useEffect(() => {
+    props.setDate(value)
+    // eslint-disable-next-line
+  }, [])
+
+  
   return (
     <div>
       <DateTimePicker
